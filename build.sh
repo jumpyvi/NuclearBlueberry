1df9d4f202a528338d38421a08d3461d043cfd5e
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2024-10-30 17:25:36 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -73,7 +73,7 @@ PACKAGES_TO_INSTALL=(
 rpm-ostree install "${PACKAGES_TO_INSTALL[@]}"
 
 rpm-ostree override remove \
-    containerd runc firefox-langpacks firefox
+    containerd firefox-langpacks firefox
 
 #### System unit file
 systemctl enable podman.socket
