76a514269e96827b556326040910225561c8081d
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2025-01-09 10:32:16 -0500

    Adds wezterm

--- build.sh
+++ build.sh
@@ -55,11 +55,25 @@ enabled=1
 enabled_metadata=1
 EOF
 
+tee /etc/yum.repos.d/docker-ce.repo <<'EOF'
+[copr:copr.fedorainfracloud.org:wezfurlong:wezterm-nightly]
+name=Copr repo for wezterm-nightly owned by wezfurlong
+baseurl=https://download.copr.fedorainfracloud.org/results/wezfurlong/wezterm-nightly/fedora-$releasever-$basearch/
+type=rpm-md
+skip_if_unavailable=True
+gpgcheck=1
+gpgkey=https://download.copr.fedorainfracloud.org/results/wezfurlong/wezterm-nightly/pubkey.gpg
+repo_gpgcheck=0
+enabled=1
+enabled_metadata=1
+EOF
+
 
 RELEASE="$(rpm -E %fedora)"
 
 PACKAGES_TO_INSTALL=(
     ghostty
+    wezterm
     nordvpn
     bootc
     containerd.io
