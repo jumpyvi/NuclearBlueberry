11d689a34b656f3c5f6861ca86ea77946d714104
JumpyVi <jumpy634@gmail.com>
2025-01-13 16:58:35 -0500

    Revert "Rebase to bluefin"
    This reverts commit de9348789b7f5a9c408463f3ab8c01a67f7745fb.

--- build.sh
+++ build.sh
@@ -3,6 +3,36 @@
 set -ouex pipefail
 
 
+tee /etc/yum.repos.d/docker-ce.repo <<'EOF'
+[docker-ce-stable]
+name=Docker CE Stable - $basearch
+baseurl=https://download.docker.com/linux/fedora/$releasever/$basearch/stable
+enabled=1
+gpgcheck=1
+gpgkey=https://download.docker.com/linux/fedora/gpg
+EOF
+
+tee /etc/yum.repos.d/vscode.repo <<'EOF'
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+tee /etc/yum.repos.d/ublue-staging.repo <<'EOF'
+[copr:copr.fedorainfracloud.org:ublue-os:staging]
+name=Copr repo for staging owned by ublue-os
+baseurl=https://download.copr.fedorainfracloud.org/results/ublue-os/staging/fedora-$releasever-$basearch/
+type=rpm-md
+skip_if_unavailable=True
+gpgcheck=1
+gpgkey=https://download.copr.fedorainfracloud.org/results/ublue-os/staging/pubkey.gpg
+repo_gpgcheck=0
+enabled=1
+enabled_metadata=1
+EOF
 
 tee /etc/yum.repos.d/nordvpn.repo <<'EOF'
 [nordvpn]
@@ -31,11 +61,54 @@ RELEASE="$(rpm -E %fedora)"
 PACKAGES_TO_INSTALL=(
     ghostty
     nordvpn
+    bootc
+    containerd.io
+    docker-buildx-plugin
+    docker-ce
+    docker-ce-cli
+    docker-compose-plugin
+    virt-install
+    libvirt
+    libvirt-daemon
+    libvirt-daemon-config-network
+    libvirt-daemon-driver-interface
+    libvirt-daemon-driver-network
+    libvirt-daemon-driver-nwfilter
+    libvirt-daemon-driver-qemu
+    libvirt-daemon-driver-secret
+    libvirt-daemon-driver-storage-core
+    libvirt-daemon-kvm
+    qemu-kvm
+    qemu-char-spice
+    qemu-device-display-virtio-gpu
+    qemu-device-display-virtio-vga
+    qemu-device-usb-redirect
+    qemu-img
+    edk2-ovmf
+    qemu-system-x86-core
     adw-gtk3-theme
+    qemu-user-binfmt
+    qemu-user-static
+    virt-manager
+    virt-viewer
+    libguestfs-tools
+    python3-libguestfs
+    virt-top
+    podman
+    podman-compose
+    distrobox
+    git-credential-libsecret
+    ifuse
+    input-remapper
+    samba
+    code
+    nautilus-open-any-terminal
     gcc
     gtk2-devel
+    android-tools 
     xhost
-    )
+    yaru-theme 
+)
 
 
 
