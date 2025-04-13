6103567883a0574e47e32b8fe2317b7efd75801d
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2025-04-13 10:44:23 -0400

    Revert "Switch to kde"
    This reverts commit c8ac2f6e4b4614bbf54c23a070f00bf3e754fc38.

--- build.sh
+++ build.sh
@@ -46,7 +46,7 @@ PACKAGES_TO_INSTALL=(
 rpm-ostree install "${PACKAGES_TO_INSTALL[@]}"
 
 rpm-ostree override remove \
-    nvtop
+    nvtop ptyxis tailscale solaar zsh simple-scan gnome-shell-extension-search-light gnome-shell-extension-tailscale-gnome-qs
 
 
 
