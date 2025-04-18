7cf5f8b37ab0780f001ab1eea2616b43ebbb4a75
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2025-04-18 19:36:29 -0400

    Switch to dnf

--- build.sh
+++ build.sh
@@ -21,20 +21,8 @@ PACKAGES_TO_INSTALL=(
 )
 
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
 # this installs a package from fedora repos
-rpm-ostree install "${PACKAGES_TO_INSTALL[@]}"
+dnf install -y "${PACKAGES_TO_INSTALL[@]}"
 
-rpm-ostree override remove \
+dnf remove -y \
     nvtop ptyxis tailscale solaar simple-scan gnome-shell-extension-search-light gnome-shell-extension-tailscale-gnome-qs
-
-
-
-### Add brew
-# curl -fsSL "https://raw.githubusercontent.com/ublue-os/bluefin/refs/heads/main/build_files/base/10-brew.sh" | bash
